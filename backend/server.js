8dc7696dc93b40caf0a96d58ce9e312b5c281d7e
melakuzeleke <melakuzeleke443@gmail.com>
2024-07-11 11:01:14 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('updateLocation', (location) => {
+        io.emit('locationUpdate', location);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
